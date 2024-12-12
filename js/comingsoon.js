f74748a318fcffbe1d142a9914a4956006e6f08e
Scr1tt <garrettbierer@gmail.com>
2024-12-12 00:49:04 -0500

    coming soon design

---
+++ js/comingsoon.js
@@ -0,0 +1,4 @@
+document.querySelector('.dropdown-btn').addEventListener('click', () => {
+    const leftNavbar = document.querySelector('.leftnavbar');
+    leftNavbar.classList.toggle('expanded'); // Toggle the "expanded" class
+});
